f3ce5852a82ba8363d082c29f88404a6d76374d3
Jeffry Gonzalez <jeff_gonzalez@progressive.com>
2021-08-26 15:20:11 -0400

    Add project files.

---
+++ StringCalculatorBlank/StringCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StringCalculatorBlank
+{
+    public class StringCalculatorTests
+    {
+
+        [Fact]
+        public void EmptyStringReturnsZero()
+        {
+            var calculator = new StringCalculator();
+
+            var answer = calculator.Add("");
+
+            Assert.Equal(0, answer);
+        } 
+    }
+}
